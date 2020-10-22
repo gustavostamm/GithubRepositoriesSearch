5141ada575d2f59375d6796604c3c4a48bbf79e6
Gustavo Stamm <gustavo.stamm@gmail.com>
2020-10-22 16:32:23 -0300

    Updates

---
+++ precache-manifest.054d6e7647e0062a0d51a010bbf5778f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b59ae8dc8408e3518b38eff05524bf",
+    "url": "/GithubRepositoriesSearch/index.html"
+  },
+  {
+    "revision": "0726db28d134f83044d5",
+    "url": "/GithubRepositoriesSearch/static/js/2.12dc1a67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GithubRepositoriesSearch/static/js/2.12dc1a67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac7c7b167cf0b039f31",
+    "url": "/GithubRepositoriesSearch/static/js/main.aac52853.chunk.js"
+  },
+  {
+    "revision": "b41851ac781fa4c691e7",
+    "url": "/GithubRepositoriesSearch/static/js/runtime-main.96352c5e.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/GithubRepositoriesSearch/static/media/githubBackground.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/GithubRepositoriesSearch/static/media/logo.04f6798f.svg"
+  }
+]);
